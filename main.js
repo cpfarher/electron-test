ea8aac1d40d926b3851b96fec4861df4ee2d0f61
cpfarher <c.pfarher@gmail.com>
2018-04-23 11:14:04 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const electron= require('electron')
+const {app, BrowserWindow} = electron
+
+const path = require('path')
+const url = require('url')
+
+let win
+
+function createWindow() {
+	win = new BrowserWindow ({width:800,height:600})
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file',
+		slashes: true
+	}))
+	win.webContents.openDevTools()
+}
+
+exports.openWindow = () => {
+	win = new BrowserWindow ({width:400,height:200})
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'first_page.html'),
+		protocol: 'file',
+		slashes: true
+	}))
+}
+
+app.on('ready', createWindow)
